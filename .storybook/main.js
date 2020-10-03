995f2bc56866d59d21db6701d9e661534ef9f894
DmitryMarkov <forall@stylive.com>
2020-10-03 14:27:48 +0200

    chore: successful attempt to upgrade to sb6

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,7 @@
 module.exports = {
-  stories: ['../src/**/*.stories.@(js|mdx)'],
-  addons: [
-    '@storybook/addon-knobs',
-    {
-      name: '@storybook/addon-docs',
-      options: { configureJSX: true },
-    },
+  stories: [
+    '../src/**/*.stories.mdx',
+    '../src/**/*.stories.@(js|jsx|ts|tsx)',
   ],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
 }
